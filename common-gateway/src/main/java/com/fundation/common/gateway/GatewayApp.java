53c25f4f4dfaf0667f6f2cd1ed2d37474bc81545
live <d7wywbm@dingtalk.com>
2022-07-05 17:14:08 +0800

    包名调整

---
+++ common-gateway/src/main/java/com/fundation/common/gateway/GatewayApp.java
@@ -0,0 +1,16 @@
+package com.fundation.common.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author
+ * @date 2022-07-05 16:57
+ */
+@SpringBootApplication
+public class GatewayApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApp.class, args);
+    }
+}
