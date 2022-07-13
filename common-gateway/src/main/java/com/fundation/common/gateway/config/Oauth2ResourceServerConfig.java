85b7ce19290b7bfbce2cd7de7e5daab4f563d938
live <d7wywbm@dingtalk.com>
2022-07-13 14:22:05 +0800

    oauth2资源中心密钥调整

--- common-gateway/src/main/java/com/fundation/common/gateway/config/Oauth2ResourceServerConfig.java
+++ common-gateway/src/main/java/com/fundation/common/gateway/config/Oauth2ResourceServerConfig.java
@@ -1,5 +1,6 @@
 package com.fundation.common.gateway.config;
 
+import cn.hutool.core.codec.Base64;
 import cn.hutool.core.convert.Convert;
 import cn.hutool.core.io.IoUtil;
 import com.foundation.common.core.config.IgnoreProperties;
@@ -35,7 +36,6 @@ import java.security.NoSuchAlgorithmException;
 import java.security.interfaces.RSAPublicKey;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.X509EncodedKeySpec;
-import java.util.Base64;
 
 /**
  * @author
@@ -109,7 +109,7 @@ public class Oauth2ResourceServerConfig {
     public ReactiveJwtDecoder jwtDecoder() throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
         Resource resource = new ClassPathResource("public.key");
         InputStream is = resource.getInputStream();
-        byte[] publicKeyBytes = Base64.getDecoder().decode(IoUtil.read(is).toString());
+        byte[] publicKeyBytes = Base64.decode(IoUtil.read(is).toString());
         X509EncodedKeySpec keySpec = new X509EncodedKeySpec(publicKeyBytes);
         KeyFactory keyFactory = KeyFactory.getInstance("RSA");
         RSAPublicKey rsaPublicKey = (RSAPublicKey) keyFactory.generatePublic(keySpec);
